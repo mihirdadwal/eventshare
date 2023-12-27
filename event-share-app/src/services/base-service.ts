e773aaf926a0648b7c409daf83d09a1840794207
Mihir Dadwal <dadwal.m@northeastern.edu>
2023-12-27 01:33:41 -0500

    updated base-service.js

--- event-share-app/src/services/base-service.ts
+++ event-share-app/src/services/base-service.ts
@@ -1,5 +1,5 @@
 
-const baseURL = 'http://localhost:3002';
+const baseURL = 'https://eventshare-nc92.onrender.com';
 export const search = async<T>(path: string, params: any): Promise<T[]> => {
     const query: URLSearchParams = new URLSearchParams(params);
     const response = await fetch(baseURL+path+query,{
